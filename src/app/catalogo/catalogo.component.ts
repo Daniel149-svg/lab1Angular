b01ad9d9d02c08f3a04344e37c238d5c726c9a17
daniel trujillo <oscartrujillo159@gmail.com>
2024-08-19 19:25:18 -0600

    laboratorio1

---
+++ src/app/catalogo/catalogo.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalogo.component.html',
+  styleUrl: './catalogo.component.css'
+})
+export class CatalogoComponent {
+
+  nombre1:string = "Tania Yanet Rosales";
+  nombre2:string = "Andres Alfaro";
+  nombre3:string = "Elias Enoc Carreno";
+
+  
+}
+
